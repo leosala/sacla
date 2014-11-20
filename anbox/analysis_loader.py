d133ed80fe2174330bec7cc8f767f4501e8ab3e8
Sala Leonardo <leonardo.sala@psi.ch>
2014-11-20 10:14:01 +0100

    first upload

---
+++ anbox/analysis_loader.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import glob
+import plugins
+# import base
+# import array
+
+# print listdir()
+# __all__ = ["base", "array"]
+
+
+def get_plugins_list():
+    plugins_list = []
+    #plugins_dir = os.path.abspath("plugins") + "/*.py"
+    #print os.path.split(__file__)
+    plugins_dir = os.path.join(os.path.split(__file__)[0], "plugins/*.py")
+    for f in glob.glob(plugins_dir):
+        if os.path.isfile(f) and not os.path.basename(f).startswith('_'):
+            plugins_list.append(os.path.basename(f)[:-3])
+    return plugins_list
+
+
+def load(htype):
+    try:
+        s_type = getattr(plugins, htype)  # __import__("plugins." + htype, fromlist=".")
+        modules_list = [i for i in dir(s_type) if i[0] != "_"]
+        #assert(len(modules_list) == 1)
+        an_plug = getattr(s_type, modules_list[0])  # __import__("plugins." + htype + "." + modules_list[0], fromlist=".")
+    except:
+        print "Cannot load plugin %s" % htype
+        sys.exit(-1)
+    return an_plug()
+
+
+if __name__ == "__main__":
+    plugins_list = get_plugins_list()
+    print plugins_list
+    for p in plugins_list:
+        print p
+        algo = load(p)
+        algo.run()
---
+++ anbox/plugins/test.py
@@ -0,0 +1,7 @@
+
+
+class TestAnalysis(object):
+
+    def run(self):
+        print "I am test"
+        return 0
