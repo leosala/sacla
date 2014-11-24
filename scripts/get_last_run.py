0dcbe647173fa46598af271f8a9b9cf50830ea70
Sala Leonardo <leonardo.sala@psi.ch>
2014-11-24 08:43:29 +0100

    added file to print out last run

---
+++ scripts/get_last_run.py
@@ -0,0 +1,12 @@
+import urllib2
+
+
+url = "http://xqaccdaq01.daq.xfel.cntl.local/cgi-bin/storage/run.cgi?bl=3"
+
+
+if __name__ == "__main__":
+    doc = urllib2.urlopen(url).readlines()[80:90]
+
+    for i, l in enumerate(doc):
+        if l.find("detectors") != -1:
+            print i, int(doc[i + 4].strip().strip("</td>"))
