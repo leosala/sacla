7c4f747ab1bc9d49b3b9e5b55febbe78c580b1e6
Sala Leonardo <leonardo.sala@psi.ch>
2014-11-26 17:39:50 +0900

    first upload, live DAQ plotting

--- scripts/plot_live_daq.py
+++ scripts/plot_live_daq.py
@@ -11,8 +11,8 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-import numpy as np
 
+import json
 import logging
 logging.basicConfig(filename='tape_migration.log',
                     format="%(process)d:%(levelname)s:%(asctime)s:%(message)s",
@@ -32,18 +32,20 @@ daq_quantities = {
 #start_time = time() - 2 #- 700000
 #stop_time = start_time + 2 # 600
 
-s = "02/06/2014 11:59"
-e = "02/06/2014 12:00"
+s = "25/11/2014 11:59"
+e = "26/11/2014 12:00"
 
 
 class UpdateDAQ(object):
-    def __init__(self, ax, start_time, stop_time, daq_quantities):
+    def __init__(self, ax, start_time, stop_time, daq_quantities, plot=None, json_name=None, csv_name=None):
         self.start_time = start_time
         self.stop_time = stop_time
-        self.data_on = None
-        self.data_off = None
+        self.data = None
         self.daq_quantities = daq_quantities
-
+        self.plot = plot
+        self.json_name = json_name
+        self.csv_name = csv_name
+        
         self.line, = ax.plot([], [], linestyle='', marker="*")
         self.ax = ax
 
@@ -54,47 +56,83 @@ class UpdateDAQ(object):
         self.line.set_data([], [])
         return self.line,
 
-    def __call__(self, i):
-        #if i == 0:
-        #    return self.init()
-        
-        old_start = self.start_time
+    def update_data(self):
         old_stop = self.stop_time
 
         self.start_time = old_stop
-        self.stop_time = old_stop + 60
+        # TODO change this with NOW
+        #self.stop_time = old_stop + 60
+        self.stop_time = time()
 
         data_on_tmp = sacla_hdf5.get_daq_data(self.daq_quantities, start_time=self.start_time, stop_time=self.stop_time, cond="xfel_bl_3_shutter_1_open_valid/status = 1")
 
-        if self.data_on is not None:
+        if self.data is not None:
             for k in daq_quantities.keys():
-                self.data_on[k][1].extend(data_on_tmp[k][1])
+                self.data[k][1].extend(data_on_tmp[k][1])
         else:
-            self.data_on = data_on_tmp.copy()
+            self.data = data_on_tmp.copy()
+
+    def __call__(self, i):
+        self.update_data()
+        x = self.data[self.plot['x']][1]
+        y = self.data[self.plot['y']][1]
+
+        while len(x) != len(y):
+            if len(x) > len(y):
+                x.pop()
+            else:
+                y.pop()
+        if self.json_name is not None:
+            json_file = open(self.json_name, 'w')
+            json_dict = {}
+            json_dict["run"] = ""
+            json_dict["name"] = "-".join(daq_quantities.keys())
+            json_dict["plot_type"] = "scatter"
+            if self.plot is not None:
+                json_dict["label_x"] = self.plot['x']
+                json_dict["label_y"] = self.plot['y']
+            json_dict["data"] = []
+            json_dict["data"].append(x)
+            json_dict["data"].append(y)
+   
+            json.dump(json_dict, json_file)
+   
+            json_file.close()
+
+        if self.csv_name is not None:
+            import csv
+            with open(self.csv_name, 'wb') as csvfile:
+                spamwriter = csv.writer(csvfile, delimiter=',',
+                                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+                spamwriter.writerow([self.plot["x"], self.plot["y"]])
+                for i, x_i in enumerate(x):
+                    spamwriter.writerow([x_i, y[i]])
+            csvfile.close()
+
+        if len(x) == 0 or len(y) == 0:
+            return self.line,
 
         xmin, xmax = self.ax.get_xlim()
-        dmax = max(self.data_on["I0"][1])
-        dmin = min(self.data_on["I0"][1])
-        ymax = max(self.data_on["Mono"][1])
-        ymin = min(self.data_on["Mono"][1])
-        
+        dmax = max(x)
+        dmin = min(x)
+        ymax = max(y)
+        ymin = min(y)
+
         self.ax.set_xlim(float(dmin), float(dmax))
         self.ax.set_ylim(0.5 * float(ymin), 1.5 * float(ymax))
         #    ax.figure.canvas.draw()
-        while len(self.data_on["I0"][1]) != len(self.data_on["Mono"][1]):
-            if len(self.data_on["I0"][1]) > len(self.data_on["Mono"][1]):
-                self.data_on["I0"][1].pop()
-            else:
-                self.data_on["I0"][1].pop()
-        self.line.set_data(self.data_on["I0"][1], self.data_on["Mono"][1])
+
+        self.line.set_data(x, y)
         return self.line,
-        
+
+
 start_time = mktime(datetime.datetime.strptime(s, "%d/%m/%Y %H:%M").timetuple())
-stop_time = mktime(datetime.datetime.strptime(e, "%d/%m/%Y %H:%M").timetuple())
+stop_time = time()  # mktime(datetime.datetime.strptime(e, "%d/%m/%Y %H:%M").timetuple())
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-ud = UpdateDAQ(ax, start_time=start_time, stop_time=stop_time, daq_quantities=daq_quantities)
+ud = UpdateDAQ(ax, start_time=start_time, stop_time=stop_time, daq_quantities=daq_quantities, plot={'x': 'I0', 'y': 'Mono'}, json_name="daq.json", csv_name="daq.csv")
 anim = FuncAnimation(fig, ud, interval=1000, blit=True)
 plt.show()
 
