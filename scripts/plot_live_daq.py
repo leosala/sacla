ecb1314eb4caea583e0d271176657801165b02a6
Sala Leonardo <leonardo.sala@psi.ch>
2014-11-27 23:40:07 +0900

    not really working...

--- scripts/plot_live_daq.py
+++ scripts/plot_live_daq.py
@@ -3,7 +3,7 @@ import sys
 import os
 # loading some utils
 sys.path.append(os.environ["PWD"] + "/../")
-from utilities import sacla_hdf5
+from utilities import sacla_hdf5, beamtime_converter_201406XX
 import numpy as np
 from time import sleep, time, mktime
 import datetime
@@ -22,7 +22,7 @@ from random import randint
 
 
 class UpdateDAQ(object):
-    def __init__(self, axes, start_time, stop_time, daq_quantities, plots=None, json_name=None, csv_name=None, operation=""):
+    def __init__(self, axes, start_time, stop_time, daq_quantities, plots=None, json_name=None, csv_name=None, operation="", average=True):
         self.start_time = start_time
         self.stop_time = stop_time
         self.data = None
@@ -32,14 +32,14 @@ class UpdateDAQ(object):
         self.csv_name = csv_name
         self.operation = operation
         self.is_first = True
+        self.average = average
 
         self.ax = axes[0]
         self.ax2 = axes[1]
-        self.line, = self.ax.plot([], [],  linestyle='', marker="o")
+        self.line, = self.ax.plot([], [], linestyle='-', marker=".")
         self.line2 = [None for i in range(len(plots))]
         for i in range(len(plots)):
-            self.line2[i], = self.ax2.plot([], [], linestyle='', marker="o")
-
+            self.line2[i], = self.ax2.plot([], [], linestyle='-', marker=".")
 
         # Set up plot parameters
         self.ax.grid(True)
@@ -83,7 +83,8 @@ class UpdateDAQ(object):
                 continue
             x = self.data_list[p_i][plot['x']][1]
             y = self.data_list[p_i][plot['y']][1]
-
+            if plot['x'] == "Mono":
+                x = beamtime_converter_201406XX.convert("energy", x)
             #print x, y
             while len(x) != len(y):
                 if len(x) > len(y):
@@ -93,13 +94,16 @@ class UpdateDAQ(object):
             dfs.append(pd.DataFrame(np.asarray([x, y]).T, columns=[plot['x'], plot['y']], ))
             dfs[p_i] = dfs[p_i].set_index(plot['x'])
             dfs[p_i] = dfs[p_i].sort_index()
+            #if not self.average:
             xs.append(dfs[p_i].index.values)
             ys.append(dfs[p_i].values.flatten())
+            #else:
+            #    xs.append(dfs[p_i].index.values)
+            #    ys.append(dfs[p_i].mean().values.flatten())
 
         if self.operation == "subtract":
             dftot = dfs[1].subtract(dfs[0], fill_value=0)
             dftot = dftot.sort_index()
-
             X = dftot.index
             Y = dftot.values
         else:
@@ -134,10 +138,26 @@ class UpdateDAQ(object):
             csvfile.close()
 
         if len(X) > 0 and len(Y) > 0:
-            self.ax.set_xlim(float(min(X)), float(max(X)))
-            self.ax.set_ylim(0.5 * float(min(Y)), 1.5 * float(max(Y)))
+            #self.ax.set_xlim(0.9 * float(min(X)), 1.1 * float(max(X)))
+            self.ax.set_ylim(0.99 * float(min(Y)), 1.01 * float(max(Y)))
 
-        self.line.set_data(X.tolist(), Y.tolist())
+        if self.average:
+            dftot_avg = dftot.mean(level=0)
+            dftot_std = dftot.std(level=0)
+            avg_x = dftot_avg.index.tolist()
+            avg_y = dftot_avg[plot['y']].values.tolist()
+            std_y = dftot_std[plot['y']].values.tolist()
+
+            self.line.set_data(avg_x, avg_y)
+            error_plus = np.array(avg_y) + np.array(std_y)
+            error_minus = np.array(avg_y) - np.array(std_y)
+            self.line.set_data(avg_x, avg_y)
+            for coll in (self.ax.collections):
+                self.ax.collections.remove(coll)
+            self.ax.fill_between(avg_x, error_minus, error_plus, alpha=0.5, edgecolor='#CC4F1B', facecolor='#FF9848')
+
+        else:
+            self.line.set_data(X.tolist(), Y.tolist())
 
         for i, l in enumerate(self.line2):
             if dfs[i].empty:
@@ -155,44 +175,61 @@ class UpdateDAQ(object):
                 xmin = dfs[i].idxmin()[self.plots[i]['y']]
             if xmax < dfs[i].idxmax()[self.plots[i]['y']]:
                 xmax = dfs[i].idxmax()[self.plots[i]['y']]
-            self.ax2.set_xlim(float(xmin), float(xmax))
+            #self.ax2.set_xlim(0.9 * float(xmin), 1.1 * float(xmax))
             if ymin > dfs[i].min()[self.plots[i]['y']]:
                 ymin = dfs[i].min()[self.plots[i]['y']]
             if ymax < dfs[i].max()[self.plots[i]['y']]:
                 ymax = dfs[i].max()[self.plots[i]['y']]
-            self.ax2.set_ylim(float(ymin), float(ymax))
+            self.ax2.set_ylim(0.99 * float(ymin), 1.01 * float(ymax))
 
-            #self.ax2.set_ylim(0.5 * float(min(ys)), 1.5 * float(max(ys)))
- 
-            self.line2[i].set_data(xs[i], ys[i])
+            print dfs[i].index.values, dfs[i].values.flatten()
+            self.line2[i].set_data(dfs[i].index.values, dfs[i].values.flatten())
+            #self.line2[i].set_data(xs[i], ys[i])
 
         return self.line, self.line2[0], self.line2[1]
 
 
 if __name__ == '__main__':
+
     daq_quantities = {
-        'I0': 'xfel_bl_3_tc_spec_1/energy',
+        #'I0': 'xfel_bl_3_tc_spec_1/energy',
         'LaserOn': 'xfel_bl_lh1_shutter_1_open_valid/status',
+        #'Mono': 'xfel_bl_3_tc_mono_1_theta/position',
+        #'PD': 'xfel_bl_3_st_4_pd_user_1_fitting_peak/voltage',
+        #'APD': 'xfel_bl_3_st_3_pd_14_fitting_peak/voltage',
+        'SamplePD': 'xfel_bl_3_st_4_pd_user_9_fitting_peak/voltage',
+        'ScanPD': 'xfel_bl_3_st_4_pd_user_10_fitting_peak/voltage',
         'Mono': 'xfel_bl_3_tc_mono_1_theta/position',
-        'PD': 'xfel_bl_3_st_4_pd_user_1_fitting_peak/voltage',
-        'APD': 'xfel_bl_3_st_3_pd_14_fitting_peak/voltage',
+        'Delay': 'xfel_bl_3_st_4_motor_29/position'
+        
     }
+
+    
+    #daq_quantities = {
+    #    'I0': 'xfel_bl_3_tc_spec_1/energy',
+    #    'LaserOn': 'xfel_bl_lh1_shutter_1_open_valid/status',
+    #    'Mono': 'xfel_bl_3_tc_mono_1_theta/position',
+    #    'PD': 'xfel_bl_3_st_4_pd_user_1_fitting_peak/voltage',
+    #    'APD': 'xfel_bl_3_st_3_pd_14_fitting_peak/voltage',
+    #    'SamplePD': 'xfel_bl_3_st_4_pd_user_9_fitting_peak/voltage',
+    #}
 
     s = "2014-06-04 11:59"  # "04/06/2014 11:59"
     e = "2014-06-04 12:00"  # "04/06/2014 12:00"
 
-    start_time = time() - 3600 #mktime(datetime.datetime.strptime(s, "%Y-%m-%d %H:%M").timetuple())
-    stop_time =  time() #mktime(datetime.datetime.strptime(e, "%Y-%m-%d %H:%M").timetuple())
+    start_time = time() - 20000  #mktime(datetime.datetime.strptime(s, "%Y-%m-%d %H:%M").timetuple())
+    stop_time =  time()  #mktime(datetime.datetime.strptime(e, "%Y-%m-%d %H:%M").timetuple())
 
     fig = plt.figure()
     ax = fig.add_subplot(1, 2, 1)
     ax2 = fig.add_subplot(1, 2, 2)
 
     ud = UpdateDAQ([ax, ax2], start_time=start_time, stop_time=stop_time, daq_quantities=daq_quantities,
-                   plots=[{'x': 'I0', 'y': 'PD', 'cond': 'xfel_bl_3_shutter_1_open_valid/status = 0'},
-                          {'x': 'I0', 'y': 'PD', 'cond': 'xfel_bl_3_shutter_1_open_valid/status = 1'},],
+                   plots=[{'x': 'Mono', 'y': 'ScanPD', 'cond': 'xfel_bl_lh1_shutter_1_open_valid/status = 0'},
+                          {'x': 'Mono', 'y': 'ScanPD', 'cond': 'xfel_bl_lh1_shutter_1_open_valid/status = 1'}, ],
                    json_name="daq.json", csv_name="daq.csv",
-                   operation="subtract"
+                   operation="subtract",
+                   average = True
                    )
 
     anim = FuncAnimation(fig, ud, interval=1000, blit=True)
