76d7ced795856a33097eb2feb2ebe679a6d35c73
ebner <simon.ebner@psi.ch>
2014-11-28 17:56:16 +0900

    fixed sorting

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -14,6 +14,8 @@ def check_for_missing_files(file_path, postfix=""):
     data_files = [f for f in listdir(file_path) if (isfile(join(file_path, f)) and re.match('^[0-9]{6}'+postfix+'.h5$', f))]
     current_number = int(data_files[0].replace(postfix+'.h5', ''))
 
+    data_files.sort()
+
     missing_numbers = []
 
     # Ensure that afterwards we are not running into an endless loop
