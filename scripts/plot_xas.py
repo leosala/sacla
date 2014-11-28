955ee0bbb8e1e7830871cd56936ceb0b0eafa992
Sala Leonardo <leonardo.sala@psi.ch>
2014-11-29 01:22:32 +0900

    added script for comparing XAS plots

--- scripts/plot_xas.py
+++ scripts/plot_xas.py
@@ -32,39 +32,20 @@ def get_energy_from_theta(theta_position):
 
 def get_delay_from_pulse(pulse, t0=0):
     """"""
-    magic_factor = 6.66 / 1000.
-    return (float(pulse) * magic_factor) - t0
+    magic_factor = 6.66713134 / 1000.
+    return ((float(pulse) * magic_factor) - t0)
 
 dir = "/work/timbvd/hdf5/"
-t0 = 221 # delay t0
-#IsData = logical(IsXray(:) & DAQ.X2Up > 0.1 & DAQ.X2Up <  0.273 & DAQ.X2Down > 0.09 & DAQ.X2Down <  0.224 & DAQ.X2Left > 0.025 & DAQ.X2Left <  0.064 & DAQ.X2Right > 0.057 & #DAQ.X2Right <  0.134);
+#t0 = 221  # delay t0
 
-if __name__ == "__main__":
-    import argparse
 
-    parser = argparse.ArgumentParser()
-
-    #parser.add_argument("filename", help="Name of the .csv file with the average scan info", nargs='+')
-    parser.add_argument("-o", "--outputdir", help="output directory", action="store", default=".")
-    parser.add_argument("-p", "--plot", help="plot, do not save", action="store_true")
-    parser.add_argument("-s", "--start_run", help="start_run", action="store", default="1")
-    parser.add_argument("-e", "--end_run", help="end_run", action="store", default="9999999")
-    parser.add_argument("-t", "--scan_type", help="scan type, e.g. energy, delay, ND", action="store", default="energy")
-    parser.add_argument("-c", "--check", help="plot check quantities (photon energy etc)", action="store_true", default=False)
-
-    args = parser.parse_args()
-
-    x = []
-    y_on = []
-    y_off = []
-    y_on_off = []
-
+def compute_xas(scan_type, start_run, end_run, t0=0):
     df = None
     df_conds = None
 
-    index_name = args.scan_type
+    index_name = scan_type
 
-    for i in range(int(args.start_run), int(args.end_run) + 1):
+    for i in range(int(start_run), int(end_run) + 1):
         fname = dir + str(i) + "_nompccd.h5"
         run = fname.split("/")[-1].replace("_roi", "").replace(".h5", "").replace("_nompccd", "")
 
@@ -73,14 +54,12 @@ if __name__ == "__main__":
             tags = f["/run_" + run + "/event_info/tag_number_list"][:]
         except:
             print "Last good run was %d" % int(i - 1)
-            args.end_run = str(i - 1)
+            end_run = str(i - 1)
             break
 
         photon_energy = f["/run_" + run + "/event_info/bl_3/oh_2/photon_energy_in_eV"][:]
         is_xray = f["/run_" + run + "/event_info/bl_3/eh_1/xfel_pulse_selector_status"][:]
         is_laser = f["/run_" + run + "/event_info/bl_3/lh_1/laser_pulse_selector_status"][:]
-        is_laser_off = is_laser == 0
-        is_laser_on = is_laser == 1
         t_conv = np.vectorize(get_energy_from_theta)
         d_conv = np.vectorize(get_delay_from_pulse)
 
@@ -90,7 +69,7 @@ if __name__ == "__main__":
         mono = t_conv(np.array(f["/run_" + run + Mono][:]))
         nd = np.array(f["/run_" + run + ND])
         delay = np.array(f["/run_" + run + "/event_info/bl_3/eh_4/laser/delay_line_motor_29"][:])
-        delay = d_conv(delay, t0=221)
+        delay = d_conv(delay, t0=t0)
 
         is_data = (is_xray == 1) * (photon_energy > 9651) * (photon_energy < 9700) * (iol < 0.5) * (iou < 0.5) * (iol > 0.) * (iou > 0.) * (nd > -1)
 
@@ -124,10 +103,29 @@ if __name__ == "__main__":
             df_conds = df_conds.set_index("tags")
         else:
             df_conds = pd.concat([df_conds, pd.DataFrame({"tags": tags, "photon_energy": photon_energy, "I0up": iou, "I0down": iol, }, ).set_index("tags")])
+        
+    return df, df_conds
 
 
-    
-    df_conds.plot(subplots=True, linestyle="", marker=".", figsize=(10, 10))
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    #parser.add_argument("filename", help="Name of the .csv file with the average scan info", nargs='+')
+    parser.add_argument("-o", "--outputdir", help="output directory", action="store", default=".")
+    parser.add_argument("-p", "--plot", help="plot, do not save", action="store_true")
+    parser.add_argument("-s", "--start_run", help="start_run", action="store", default="1")
+    parser.add_argument("-e", "--end_run", help="end_run", action="store", default="9999999")
+    parser.add_argument("-t", "--scan_type", help="scan type, e.g. energy, delay, ND", action="store", default="energy")
+    parser.add_argument("-c", "--check", help="plot check quantities (photon energy etc)", action="store_true", default=False)
+
+    args = parser.parse_args()
+    df, df_conds = compute_xas(args.scan_type, args.start_run, args.end_run)
+
+    if args.check:
+        df_conds.plot(subplots=True, linestyle="", marker=".", figsize=(10, 10))
 
     fig = plt.figure(figsize=(20, 15))
     fig.suptitle("Runs %s - %s" % (args.start_run, args.end_run))
