a8bc62574693410af8bcdd6420d75067c710c002
Sala Leonardo <Leonardo.Sala@psi.ch>
2015-10-20 11:15:57 +0200

    updated

--- examples/example_pump_probe_spectra.py
+++ examples/example_pump_probe_spectra.py
@@ -9,9 +9,8 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-# loading some utils
+### loading some utils
 TOOLS_DIR = "../../tools"
-
 # Loading ImagesProcessor
 try:
     from tools.images_processor import ImagesProcessor
@@ -22,44 +21,72 @@ except:
         from tools.images_processor import ImagesProcessor
     except:
         print "[ERROR] cannot load ImagesProcessor library"
+# SACLA-specific tools     
+SACLA_LIB = "../"
+sys.path.append(SACLA_LIB)
+import utilities as ut
+# specific converters for the 2014-11 data taking. These should be customized per each beamtime!
+from utilities import beamtime_converter_201411XX as sacla_converter
 
 
-create_calib = True
-#run = "206162"
-run = "259408"
-#run = "206183"
-DIR = "/media/sala/Elements/Data/Sacla/"
+# Define SACLA quantities - they can change from beamtime to beamtime
+daq_labels = {}
+daq_labels["I0_down"] = "event_info/bl_3/eh_4/photodiode/photodiode_I0_lower_user_7_in_volt"
+daq_labels["I0_up"] = "event_info/bl_3/eh_4/photodiode/photodiode_I0_upper_user_8_in_volt"
+daq_labels["TFY"] = "event_info/bl_3/eh_4/photodiode/photodiode_sample_PD_user_9_in_volt"
+daq_labels["photon_mono_energy"] = "event_info/bl_3/tc/mono_1_position_theta"
+daq_labels["delay"] = "event_info/bl_3/eh_4/laser/delay_line_motor_29"
+daq_labels["ND"] = "event_info/bl_3/eh_4/laser/nd_filter_motor_26"
+daq_labels["photon_sase_energy"] = "event_info/bl_3/oh_2/photon_energy_in_eV"
+daq_labels["x_status"] = "event_info/bl_3/eh_1/xfel_pulse_selector_status"
+daq_labels["x_shut"] = "event_info/bl_3/shutter_1_open_valid_status"
+daq_labels["laser_status"] = "event_info/bl_3/lh_1/laser_pulse_selector_status"
+daq_labels["tags"] = "event_info/tag_number_list"
 
+# run number
+run = "257722"
+DIR = "/home/sala/Work/Data/SACLA/"  # directory containing data
+dark_fname = None  # eventual file containing dark corrections
+fname = DIR + run + "_roi.h5"
+
+# selections
+sel = "(x_shut == 1) & (x_status == 1) & (I0_up > 0.01) & (I0_down > 0.01) & (ND > -1) & (photon_mono_energy) > 9"
+
+# open data files
 f = h5py.File(DIR + run + "_roi.h5")
+if dark_fname is not None:
+    f_calib = h5py.File(dark_fname, "r")
+    corr = f_calib["/dark1"][:]
+    f_calib.close()
 
+# get DAQ quantities (only scalars)
+df, filenames = ut.analysis.get_data_daq(fname, daq_labels, sacla_converter, t0=0, selection=sel)
 
-tag_list = f["/run_" + run + "/event_info/tag_number_list"][:]
+# get laser on/off tags
+is_laser_on_tags = df[df.is_laser == 1].index.tolist()
+is_laser_off_tags = df[df.is_laser == 0].index.tolist()
 
-# corr = None  # f_calib[run + "/MPCCD-1N0-M01-001/per_pixel_calib"][:]
-if create_calib:
-    f_calib = h5py.File(run + "_mpccd_calib.h5", "w")
-    corr = ut.analysis.per_pixel_correction_sacla(f["/run_" + run + "/detector_2d_1/"], tag_list, thr=70)
-    calib_dst = f_calib.create_dataset(run + "/MPCCD-1N0-M01-001/per_pixel_calib", data=corr)
-else:
-    f_calib = h5py.File(run + "_mpccd_calib.h5", "r")
-    corr = f_calib[run + "/MPCCD-1N0-M01-001/per_pixel_calib"][:]
-
-f_calib.close()
-is_laser = f["/run_" + run + "/event_info/bl_3/lh_1/laser_pulse_selector_status"][:]
-
-is_laser_off = is_laser == 0
-is_laser_on = is_laser == 1
-print is_laser_off.shape, is_laser_on.shape
-
-init = time()
+# get spectra from Von Hamos, using laser on / off tags
 #roi = [[0, 1024], [325, 335]]  # X, Y
-roi = []
-[sum_image_on, spectrum_on], [sum_image_off, spectrum_off] = ut.cython_utils.get_spectrum_sacla(f["/run_" + run + "/detector_2d_1/"], tag_list, corr=corr, roi=roi, masks=[[is_laser_on], [is_laser_off]])
-print sum_image_on, spectrum_on
-print "time:", time() - init
+ap = ImagesProcessor(facility="SACLA")
+ap.add_analysis('get_projection', args={"axis": 1})
+ap.add_analysis('get_mean_std')
+ap.set_dataset('/run_%s/detector_2d_1' % run)
+ap.add_preprocess("set_thr", args={"thr_low": 65})
+
+# get the total spectra
+results_on = ap.analyze_images(fname, tags=is_laser_on_tags)
+spectrum_on = results_on["get_projection"]["spectra"].sum(axis=0)
+results_off = ap.analyze_images(fname, tags=is_laser_off_tags)
+spectrum_off = results_off["get_projection"]["spectra"].sum(axis=0)
+
 spectrum_off = spectrum_off / spectrum_off.sum()
 spectrum_on = spectrum_on / spectrum_on.sum()
 
+# this is the average image from the Von Hamos
+sum_image_on = results_on["get_mean_std"]["images_mean"]
+
+# Plot!
 plt.subplot(1, 2, 1)
 plt.imshow(sum_image_on)
 plt.subplot(1, 2, 2)
@@ -67,5 +94,5 @@ plt.plot(spectrum_off, label="laser off")
 plt.plot(spectrum_on, label="laser on")
 plt.plot(spectrum_on - spectrum_off, label="on - off", color="k")
 plt.legend(loc="best")
-
+plt.tight_layout()
 plt.show()
--- utilities/analysis.py
+++ utilities/analysis.py
@@ -5,6 +5,7 @@ from time import time
 import sys
 import h5py
 import pydoc
+import pandas as pd
 
 def rebin(a, *args):
     """
@@ -16,6 +17,85 @@ def rebin(a, *args):
     #print factor
     evList = ['a.reshape('] + ['args[%d],factor[%d],' % (i, i) for i in range(lenShape)] + [')'] + ['.mean(%d)' % (i + 1) for i in range(lenShape)]
     return eval(''.join(evList))
+
+
+def get_data_daq(filenames, daq_labels, sacla_converter, t0=0, selection=""):
+    # create a DataFrame
+    df_orig = pd.DataFrame(columns=daq_labels.keys(), )
+    failed_filenames = []
+
+    if isinstance(filenames, str):
+        filenames = [filenames, ]
+
+    filenames = sorted(filenames)
+    for fname in filenames:
+        mydict = {}  # temporary dict, where to store data
+        
+        try:
+            f = h5py.File(fname, "r")
+            run = f.keys()[1].split("_")[1]  # this assumes 1 run per file, as run_XXXXX
+            main_dset = f[f.keys()[1]]  
+        except:
+            print "Error loading file %s: %s" % (fname, sys.exc_info()[1])
+            failed_filenames.append(fname)
+            continue
+
+        # Loading data from the specified datasets
+        for k, v in daq_labels.iteritems():
+            if k == "delay":
+                # delays are in motor steps
+                mydict[k] = sacla_converter.convert("delay", main_dset[v][:], t0=t0)
+            elif k == "photon_mono_energy":
+                # mono energy settings are in motor steps
+                mydict[k] = sacla_converter.convert("energy", main_dset[v][:])
+            elif k == "photon_sase_energy":
+                mydict[k + "_mean"] = main_dset[v][:].mean()
+            else:
+                mydict[k] = main_dset[v][:]
+
+        tmp_df = pd.DataFrame(data=mydict)
+        tmp_df["run"] = run
+        # Append the data to the dataframe
+        df_orig = df_orig.append(tmp_df)
+
+    if df_orig.size == 0:
+        return df_orig, filenames
+        
+    # removing failed runs
+    for r in failed_filenames:
+        filenames.remove(r)
+
+    # round mono energy and delay
+    df_orig.photon_mono_energy = np.round(df_orig.photon_mono_energy.values, decimals=4)
+    df_orig.delay = np.round(df_orig.delay.values, decimals=1)
+
+    # create total I0 and absorption coefficients
+    df_orig["I0"] = df_orig.I0_up + df_orig.I0_down
+    df_orig["is_laser"] = (df_orig['laser_status'] == 1)
+
+    # set tag number as index
+    df_orig = df_orig.set_index("tags")
+
+    # filtering out garbage
+    if selection != "":
+        df = df_orig.query(selection)
+    else:
+        df = df_orig
+
+    # print selection efficiency
+    print "\nSelection efficiency"
+    sel_eff = pd.DataFrame( {"Total":df_orig.groupby("run").count().ND, 
+                             "Selected": df.groupby("run").count().ND, 
+                             "Eff.": df.groupby("run").count().ND / df_orig.groupby("run").count().ND})
+    print sel_eff
+
+    # checking delay settings
+    g = df.groupby(['run', 'delay', 'photon_mono_energy'])
+    print "\nEvents per run and delay settings"
+    print g.count().I0
+    
+    return df, filenames
+    
 
 
 def per_pixel_correction_chunked(data, thr, chk_size=300):
